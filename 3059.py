57663d924b723e466b18df1739d034ba1f9ba588
DariaLotareva <54505528+DariaLotareva@users.noreply.github.com>
2019-08-29 14:46:34 +0300

    Homework_3

---
+++ 3059.py
@@ -0,0 +1,5 @@
+n=int(input())
+p=1
+while p<n+1:
+    print(p)
+    p=p*2
---
+++ task.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(1,n+1):
+
+    i=str(i)
+    count=len(i)
+    i=int(i)
+    if i%count==0:
+         print(i)
